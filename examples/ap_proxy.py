f89f16260a2014342b7ec5a328880649c94d5365
Eivind Uggedal <eivind@uggedal.com>
2011-11-10 16:49:31 +0100

    Proxy for switching out the ap.no stylesheet.

---
+++ examples/ap_proxy.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+import SocketServer
+import urllib
+import sys
+
+PORT = 8080
+URL_PREFIX = "http://www.aftenposten.no"
+
+CSS_MOCK = None
+
+class ApProxy(SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path.startswith("/css"):
+            self.path = "/%s" % CSS_MOCK
+            f = self.send_head()
+            if f:
+                self.copyfile(f, self.wfile)
+                f.close()
+        else:
+            url = URL_PREFIX + self.path
+            f = urllib.urlopen(url)
+            if f:
+                self.copyfile(f, self.wfile)
+                f.close()
+
+    def log_request(self, code='-', size='-'):
+        self.log_message('"%s" %s %s',
+                         "GET %s" % self.path, str(code), str(size))
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print "Usage: %s myfile.css" % sys.argv[0]
+        sys.exit(1)
+
+    CSS_MOCK = sys.argv[1]
+
+    httpd = SocketServer.TCPServer(("", PORT), ApProxy)
+    httpd.serve_forever()
